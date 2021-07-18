9dff1306e965ed41659b831fa494e9582807e3a9
ARS <closedAI@mail.ru>
2021-07-18 10:02:05 +0300

    progress bar

---
+++ Assets/Scripts/ProgressBarController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarController : MonoBehaviour
+{
+    [SerializeField] private Image progressBar;
+    //[SerializeField, Range(0f, 1f)]
+    private float val;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        val = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progressBar.fillAmount = val;
+    }
+
+    public void SetVal(float value)
+    {
+        val = value;
+    }
+}
